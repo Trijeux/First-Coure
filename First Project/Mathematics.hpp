e1a8ce26eb8a7a63424806e954206d7c9f9e30fa
DESKTOP-CFOLVV6\antho <anthonybarman2003@gmail.com>
2024-09-04 13:52:46 +0200

    Upgrade

---
+++ First Project/Mathematics.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+inline int AddNumbers(const int a, const int b)
+{
+	return a + b;
+}
+
+inline int AddNumbers(const int a, const int b, const int c)
+{
+	return a + b + c;
+}
---
+++ First Project/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "Mathematics.hpp"
+
+void main()
+{
+    std::cout << "Hello World!\n";
+
+    std::cout << "Result 2 Number " << AddNumbers(2, 2) << std::endl;
+
+    std::cout << "Result 3 Number " << AddNumbers(2,2,3) << std::endl;
+}
